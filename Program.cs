3989c947aeac460a0812685177465d6fe8604c97
Jennonthan <lan18022@byui.edu>
2022-04-25 10:28:18 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace game
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string last = Console.ReadLine();
+
+            Console.Write($"Your name is {last}, {first} {last}.");
+        }
+    }
+}
+
+
